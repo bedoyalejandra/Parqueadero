e84619defc03ab95f8b2a381746044fe0b2aaf79
bedoyalejandra <alejandrabedoya00@gmail.com>
2020-03-14 12:45:55 -0500

    Navbar

---
+++ assets/index.js
@@ -0,0 +1,7 @@
+export default {
+    data() {
+      return {
+        mainProps: { blank: true, width: 75, height: 55, class: 'm1' }
+      }
+    }
+  }
---
+++ pages/index.vue
@@ -0,0 +1,65 @@
+<template>
+	<div class="navbar">
+		<div class="navbar2">
+			<b-img src="../images/logo.png" height="60"></b-img>
+			<div class="containerButton"> 
+				<div class="textButton"> 
+					Lista de Vehículos
+				</div>
+			</div>
+
+			<div class="containerButton"> 
+				<div class="textButton"> 
+					Crear Vehículo
+				</div>
+			</div>
+		</div>
+	</div>
+    
+</template>
+
+<script src="../assets/index.js"/>
+
+<style scoped>
+
+	.navbar{
+		background-color: #ff8d00;
+	}
+
+	.navbar2{
+		
+		display: flex;
+		flex-direction: row;
+		flex-wrap: nowrap;
+		justify-content: flex-start;
+		align-items: center;
+		align-content: stretch;
+	}
+	
+
+	.logo{
+		color: white;
+		font-family: 'Rubik Mono One', sans-serif;
+		font-size: 30px;
+	}
+
+	.containerButton{
+		border: 2px solid;
+		border-color: #fff;
+		border-radius: 50px;
+		-webkit-border-radius: 5px 10px;  /* Safari  */
+  		-moz-border-radius: 5px 10px;     /* Firefox */
+		margin-left: 10px;
+		margin-right: 10px;
+	}
+
+	.textButton{
+		font-family: 'Anton', sans-serif;
+		font-size: 20px;
+		color: #FFF;
+		margin-left: 10px;
+		margin-right: 10px;
+	}
+
+
+</style>
